2699ed4cac1f8c25778924daa4bf9cb1ced39112
Person 2 <abc@gmail.com>
2023-04-06 17:39:19 -0600

    Add message in log
    This is for test.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -42,9 +42,9 @@ export function register(config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
-            'This web app is being served cache-first by a service ');
+            'This web app is being served cache-first by a service it looks good');
           console.log(
-            'This web app is being served cache-first by a service ' +
+            'This web app is being served cache-first by a service test ' +
               'worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
         });
